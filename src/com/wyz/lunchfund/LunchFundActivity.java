88939a04583be7f87440597782d82a355e453e66
Wu Yongzheng <wuyongzheng@gmail.com>
2014-05-09 13:04:30 +0800

    first time compile pass

--- src/com/wyz/lunchfund/LunchFundActivity.java
+++ src/com/wyz/lunchfund/LunchFundActivity.java
@@ -1,15 +1,193 @@
 package com.wyz.lunchfund;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.*;
+import android.view.View;
+import java.util.*;
 
 public class LunchFundActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private PersistentState pstate;
+	Set<String> checkedPeople = new HashSet<String>();
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	private void redraw ()
+	{
+		Button button;
+
+		button = (Button)findViewById(R.id.lunch);
+		button.setEnabled(checkedPeople.size() > 0);
+		button = (Button)findViewById(R.id.transfer);
+		button.setEnabled(true);
+		button = (Button)findViewById(R.id.addperson);
+		button.setEnabled(true);
+		button = (Button)findViewById(R.id.undo);
+		button.setEnabled(pstate.hasHistory());
+		button = (Button)findViewById(R.id.email);
+		button.setEnabled(checkedPeople.size() > 0);
+
+		((LinearLayout)findViewById(R.id.peoplelayout)).removeAllViews();
+		for (PersistentState.Person person : pstate.listPeople()) {
+			final String name = person.name;
+			CheckBox cbox = new CheckBox(this);
+			cbox.setText(name + " (" + person.balance + ")");
+			cbox.setChecked(checkedPeople.contains(name));
+			cbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+					public void onCheckedChanged (CompoundButton buttonView, boolean isChecked)
+					{
+						if (isChecked)
+							checkedPeople.add(name);
+						else
+							checkedPeople.remove(name);
+						redraw();
+					}
+				});
+		}
+
+		((TextView)findViewById(R.id.logview)).setText(pstate.printHistory());
+	}
+
+	private String lunchPayer;
+	private int lunchAmount;
+	public void onLunch (View view)
+	{
+		if (checkedPeople.size() == 0)
+			return;
+		final String [] people = pstate.listPeopleNames();
+		lunchPayer = null;
+		lunchAmount = 0;
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("Lunch 1/2: Who Paied?");
+		alert.setSingleChoiceItems(people, -1, new DialogInterface.OnClickListener() {
+				public void onClick (DialogInterface dialog, int which) {
+					lunchPayer = people[which];
+				}
+			});
+		alert.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					onLunch2();
+				}
+			});
+		alert.show();
+	}
+
+	public void onLunch2 ()
+	{
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("Transfer 2/2: How much in total?");
+		final EditText input = new EditText(this);
+		alert.setView(input);
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					String value = input.getText().toString();
+					double d = 0;
+					try {
+						d = Double.parseDouble(value);
+					} catch (Exception x) {
+						Log.e("LunchFund", "onLunch2() ", x);
+					}
+					if (d == 0 || Math.abs(d) * 100 >= Integer.MAX_VALUE)
+						return;
+					pstate.apply(new PersistentState.LunchTransaction(
+							lunchPayer, (int)Math.round(d * 100), checkedPeople.toArray(new String[checkedPeople.size()])));
+					redraw();
+				}
+			});
+		alert.show();
+	}
+
+	private String [] transferPeople;
+	private int transferFrom;
+	private int transferTo;
+	private int transferAmount;
+	public void onTransfer (View view)
+	{
+		transferFrom = transferTo = -1;
+		transferAmount = 0;
+		transferPeople = pstate.listPeopleNames();
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("Transfer 1/3: From Who?");
+		alert.setSingleChoiceItems(transferPeople, -1, new DialogInterface.OnClickListener() {
+				public void onClick (DialogInterface dialog, int which) {
+					transferFrom = which;
+				}
+			});
+		alert.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					onTransfer2();
+				}
+			});
+		alert.show();
+	}
+
+	public void onTransfer2 ()
+	{
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("Transfer 2/3: To Who?");
+		alert.setSingleChoiceItems(pstate.listPeopleNames(), -1, new DialogInterface.OnClickListener() {
+				public void onClick (DialogInterface dialog, int which) {
+					transferTo = which;
+				}
+			});
+		alert.setPositiveButton("Next", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					onTransfer3();
+				}
+			});
+		alert.show();
+	}
+
+	public void onTransfer3 ()
+	{
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("Transfer 3/3: How Much?");
+		final EditText input = new EditText(this);
+		alert.setView(input);
+		alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					String value = input.getText().toString();
+					double d = 0;
+					try {
+						d = Double.parseDouble(value);
+					} catch (Exception x) {
+						Log.e("LunchFund", "onTransfer3() ", x);
+					}
+					if (d == 0 || Math.abs(d) * 100 >= Integer.MAX_VALUE)
+						return;
+					pstate.apply(new PersistentState.TransferTransaction(
+							transferPeople[transferFrom], transferPeople[transferTo],
+							(int)Math.round(d * 100)));
+					redraw();
+				}
+			});
+		alert.show();
+	}
+
+	public void onAddPerson (View view)
+	{
+		String name = "p" + new Random().nextInt(100);
+		pstate.apply(new PersistentState.AddTransaction(name, name + "@gmail.com"));
+		redraw();
+	}
+
+	public void onUndo (View view)
+	{
+		pstate.undo();
+		redraw();
+	}
+
+	public void onEmail (View view)
+	{
+	}
 }
---
+++ src/com/wyz/lunchfund/PersistentState.java
@@ -0,0 +1,239 @@
+package com.wyz.lunchfund;
+
+import android.util.Log;
+import java.util.*;
+import java.io.*;
+
+public class PersistentState
+{
+	private SortedMap<String, Person> people = new TreeMap<String, Person>();
+	private Stack<Transaction> history = new Stack<Transaction>();
+	private Stack<Transaction> undoHistory = new Stack<Transaction>();
+
+	public static PersistentState load (String text)
+	{
+		return load(new StringReader(text));
+	}
+
+	public static PersistentState load (Reader in)
+	{
+		try {
+			return _load(in);
+		} catch (Exception x) {
+			Log.e("PersistentState", "load()", x);
+			return null;
+		}
+	}
+
+	public static PersistentState _load (Reader in) throws Exception
+	{
+		PersistentState pstate = new PersistentState();
+		BufferedReader reader = new BufferedReader(in);
+		while (true) {
+			String line = reader.readLine();
+			if (line == null)
+				break;
+			line = line.trim();
+			if (line.length() == 0)
+				continue;
+			Transaction trans = loadTransaction(line);
+			pstate.apply(trans);
+		}
+		return pstate;
+	}
+
+	public void save (Writer out) throws IOException
+	{
+		PrintWriter writer = new PrintWriter(out);
+		for (Transaction trans : history) {
+			writer.println(trans.save());
+		}
+		writer.flush();
+	}
+
+	private static Transaction loadTransaction (String line)
+	{
+		String [] arr = line.split("\t");
+		if (arr[0].equals("add")) {
+			return new AddTransaction(arr[1], arr[2]);
+		} else if (arr[0].equals("delete")) {
+			return new DeleteTransaction(arr[1], arr[2]);
+		} else if (arr[0].equals("transfer")) {
+			return new TransferTransaction(arr[1], arr[2], Integer.parseInt(arr[3]));
+		} else if (arr[0].equals("lunch")) {
+			String [] eaters = new String [arr.length - 3];
+			System.arraycopy(arr, 3, eaters, 0, eaters.length);
+			return new LunchTransaction(arr[1], Integer.parseInt(arr[2]), eaters);
+		} else {
+			throw new RuntimeException("unknown transaction " + arr[0]);
+		}
+	}
+
+	public static class Person {
+		public String name;
+		public String email;
+		public int balance;
+		public Person (String name, String email, int balance) {
+			this.name = name;
+			this.email = email;
+			this.balance = balance;
+		}
+	}
+
+	public static abstract class Transaction {
+		public abstract void apply (PersistentState pstate);
+		public abstract void undo (PersistentState pstate);
+		public abstract String save ();
+	}
+
+	public static class AddTransaction extends Transaction {
+		private String name;
+		private String email;
+		public AddTransaction (String name, String email)
+		{
+			this.name = name;
+			this.email = email;
+		}
+		public void apply (PersistentState pstate)
+		{
+			if (pstate.people.containsKey(name))
+				throw new RuntimeException("AddTransaction.apply(): exist user " + name);
+			pstate.people.put(name, new Person(name, email, 0));
+		}
+		public void undo (PersistentState pstate)
+		{
+			if (!pstate.people.containsKey(name))
+				throw new RuntimeException("AddTransaction.undo(): no user " + name);
+			pstate.people.remove(name);
+		}
+		public String save () {return "add\t" + name + "\t" + email;}
+	}
+
+	public static class DeleteTransaction extends Transaction {
+		private String name;
+		private String email;
+		public DeleteTransaction (String name, String email)
+		{
+			this.name = name;
+			this.email = email;
+		}
+		public void apply (PersistentState pstate)
+		{
+			if (!pstate.people.containsKey(name))
+				throw new RuntimeException("DeleteTransaction.apply(): no user " + name);
+			pstate.people.remove(name);
+		}
+		public void undo (PersistentState pstate)
+		{
+			if (pstate.people.containsKey(name))
+				throw new RuntimeException("DeleteTransaction.undo(): exist user " + name);
+			pstate.people.put(name, new Person(name, email, 0));
+		}
+		public String save () {return "delete\t" + name + "\t" + email;}
+	}
+
+	public static class TransferTransaction extends Transaction {
+		private String from;
+		private String to;
+		private int amount;
+		public TransferTransaction (String from, String to, int amount)
+		{
+			if (from.equals(to) || amount <= 0)
+				throw new RuntimeException("Invalid parameter for TransferTransaction");
+			this.from = from;
+			this.to = to;
+			this.amount = amount;
+		}
+		public void apply (PersistentState pstate)
+		{
+			pstate.people.get(from).balance += amount;
+			pstate.people.get(to).balance -= amount;
+		}
+		public void undo (PersistentState pstate)
+		{
+			pstate.people.get(from).balance -= amount;
+			pstate.people.get(to).balance += amount;
+		}
+		public String save () {return "transfer\t" + from + "\t" + to + "\t" + amount;}
+	}
+
+	public static class LunchTransaction extends Transaction {
+		private String payer;
+		private int amount;
+		private String [] eaters;
+		public LunchTransaction (String payer, int amount, String [] eaters)
+		{
+			if (eaters.length == 0 || amount <= 0)
+				throw new RuntimeException("Invalid parameter for LunchTransaction");
+			this.payer = payer;
+			this.amount = amount;
+			this.eaters = eaters;
+		}
+		public void apply (PersistentState pstate)
+		{
+			for (String eater : eaters)
+				pstate.people.get(eater).balance -= amount / eaters.length; //TODO round
+			pstate.people.get(payer).balance += amount;
+		}
+		public void undo (PersistentState pstate)
+		{
+			for (String eater : eaters)
+				pstate.people.get(eater).balance += amount / eaters.length;
+			pstate.people.get(payer).balance -= amount;
+		}
+		public String save () {
+			StringBuilder sb = new StringBuilder().append("lunch\t").append(payer).append("\t" + amount);
+			for (String eater : eaters)
+				sb.append(eater).append("\t");
+			return sb.toString();
+		}
+	}
+
+	public boolean hasHistory ()
+	{
+		return history.size() > 0;
+	}
+
+	public Iterable<Person> listPeople ()
+	{
+		return people.values();
+	}
+
+	public String[] listPeopleNames ()
+	{
+		return people.keySet().toArray(new String[people.size()]);
+	}
+
+	public String printHistory ()
+	{
+		StringBuilder sb = new StringBuilder();
+		for (Transaction trans : history)
+			sb.append(trans.save() + "\n");
+		return sb.toString();
+	}
+
+	public void apply (Transaction trans)
+	{
+		undoHistory.clear();
+		trans.apply(this);
+		history.push(trans);
+	}
+
+	public void undo ()
+	{
+		if (history.size() == 0)
+			throw new RuntimeException("PersistentState.undo while history is empty");
+		Transaction trans = history.pop();
+		trans.undo(this);
+		undoHistory.push(trans);
+	}
+
+	public void redo ()
+	{
+		if (undoHistory.size() == 0)
+			throw new RuntimeException("PersistentState.redo while undoHistory is empty");
+		Transaction trans = undoHistory.pop();
+		trans.apply(this);
+		history.push(trans);
+	}
+}
